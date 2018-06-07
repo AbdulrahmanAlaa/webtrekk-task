6684f5019856fa6801cf85b2840d20ae4c5a1ab9
abdulrahman alaa <abdelraman.bashir@vodafone.com>
2018-06-07 02:13:29 +0200

    linking the front end with the backend

---
+++ src/app/config/api-route.ts
@@ -0,0 +1,10 @@
+import { environment } from "../../environments/environment";
+
+export const API_ROUTES = {
+    // CUSTOMERS MODULE
+    'GET_CUSTOMERS':`${environment.apiBaseUrl}/customers`,
+    'GET_CUSTOMER_BY_ID':(customer_id)=>`${environment.apiBaseUrl}/customers/${customer_id}`,
+    'DELETE_CUSTOMER_BY_ID':(customer_id)=>`${environment.apiBaseUrl}/customers/${customer_id}`,
+    'CREATE_CUSTOMER':`${environment.apiBaseUrl}/customers`,
+    'UPDATE_CUSTOMER':`${environment.apiBaseUrl}/customers`
+}
