818447d11a08f193fef4599c6f204d1568903d97
abdulrahman alaa <abdelraman.bashir@vodafone.com>
2018-08-20 01:08:24 +0200

    configuring the auth middleware to validate auth users for customer api using passport

--- Server/configurations/passport.js
+++ Server/configurations/passport.js
@@ -1,11 +1,15 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Users = require('../models/user.model');
-
+const { ExtractJwt, Strategy:JWTStrategy } = require('passport-jwt');
+const config = require('./defines');
 passport.use(
     // Defining the passport email/password validation strategy
-    new LocalStrategy((email, password, done) => {
-       return Users.getOne(email, password).then((user) => {
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    }, (email, password, done) => {
+        return Users.getOne(email, password).then((user) => {
             if (!user) {
                 return done(null, false, { message: 'Incorrect email or password.' });
             } else {
@@ -14,3 +18,12 @@ passport.use(
         }).catch((error) => done(error));
     })
 );
+passport.use(
+    new JWTStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey   : config.JWT.SECRETE
+    },(jwtPayload, cb)=>{
+        // console.log('jwtPayload',jwtPayload);
+        return cb(null, jwtPayload);
+    })
+)
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 const customersRoutes = require('./customers');
 const authRoutes = require('./auth');
+const passport = require('passport');
 
 /** Initializing Customer Module Routes */
-router.use('/customers', customersRoutes);
+router.use('/customers', passport.authenticate('jwt', { session: false }), customersRoutes);
 router.use('/auth', authRoutes);
 
 module.exports = router;
